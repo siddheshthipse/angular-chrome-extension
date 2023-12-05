844a8dcdaf18d4d6feecb8e384e460a6284c0ba9
Siddhesh Nilesh Thipse <tsiddhesh@kaartech.com>
2023-12-05 19:20:13 +0530

    feat:added a console.log statement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-chrome-extension';
+export class AppComponent implements OnInit {
+  title: string = "Angular Chrome Extension";
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log("Hello from the Angular Chrome Extension")      
+  }
 }
