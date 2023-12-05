39cd18522c09b815d6a0003311fc96eb245e5502
Siddhesh Nilesh Thipse <tsiddhesh@kaartech.com>
2023-12-05 22:54:46 +0530

    feat:added a form field for color input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string = "Angular Chrome Extension";
-  constructor() { }
+
+  myForm: FormGroup | any;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     console.log("Hello from the Angular Chrome Extension")
+    this.myForm = this.formBuilder.group({
+      inputField: ['', Validators.required]
+    });
   }
 
-  paintItRed(): void {
+  paintItRed(color: string): void {
     const updateBackgroundColor = (color: string) => document.body.style.backgroundColor = color;
 
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
@@ -24,5 +31,15 @@ export class AppComponent implements OnInit {
         args: ['red']
       });
     });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      const color: string = this.myForm.value;
+      this.paintItRed(color)
+    } else {
+      this.myForm.markAllAsTouched();
+    }
   }
 }
