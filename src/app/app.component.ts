71d895b9cbdc6fb5a730e888e1c1a5d6efdaddae
Siddhesh Nilesh Thipse <tsiddhesh@kaartech.com>
2023-12-05 22:07:45 +0530

    feat:color change logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string = "Angular Chrome Extension";
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
-    console.log("Hello from the Angular Chrome Extension")      
+    console.log("Hello from the Angular Chrome Extension")
+  }
+
+  paintItRed(): void {
+    const updateBackgroundColor = (color: string) => document.body.style.backgroundColor = color;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      console.log("All the Tabs", tabs)
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id! },
+        func: updateBackgroundColor,
+        args: ['red']
+      });
+    });
   }
 }
