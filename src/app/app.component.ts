1909fe54cfec5addb1e2edee5da263335986a09b
Siddhesh Nilesh Thipse <tsiddhesh@kaartech.com>
2023-12-06 13:17:29 +0530

    fix:added a delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log("Hello from the Angular Chrome Extension")
 
-    const promptTextarea = document.getElementById('prompt-textarea');
-    console.log("Textarea for chatgpt", promptTextarea)
-
-    if (promptTextarea) {
-      promptTextarea.addEventListener('keyup', this.onKeyUp.bind(this));
-    }
+    setTimeout(() => {
+      console.log("After timeout")
+      const promptTextarea = document.getElementById('prompt-textarea');
+      if (promptTextarea) {
+        promptTextarea.addEventListener('keyup', this.onKeyUp.bind(this));
+      }
+    }, 2000); // Adjust the delay as needed
 
     this.myForm = this.formBuilder.group({
       inputField: ['', Validators.required]
